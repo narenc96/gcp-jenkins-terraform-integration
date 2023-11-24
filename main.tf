9ae949598f7eacaec628d94145dc7ac730274bf1
NAREN <84628087+narenc96@users.noreply.github.com>
2023-11-24 23:55:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "google" {
   project     = "jenkins-gke-267"
   region      = "us-central1"
-  CREDENTIALS_ID = '73eb42fa-6eae-4eb1-8402-83948dd827f7'
+  credentials_id = '73eb42fa-6eae-4eb1-8402-83948dd827f7'
 
 }
 
