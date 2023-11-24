9f8a9e1bd6666d9eedb7e7f8bbaef445a7993108
NAREN <84628087+narenc96@users.noreply.github.com>
2023-11-24 23:48:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "google" {
-  project     = "tcb-project-371706"
+  project     = "jenkins-gke-267"
   #credentials = file("tcb-project-371706-b114ce01e529.json")
-  region      = "us-east1"
+  region      = "us-central1"
 
 }
 
